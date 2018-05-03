d6eddf56c1ae9d92a054a96d10cc29ac719f712d
Sebastian Huber <sebastian_christoph_huber@yahoo.de>
2018-05-03 23:23:19 +0200

    correct linting errors

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,6 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -16,7 +18,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create user" do
-    assert_difference('User.count') do
+    assert_difference("User.count") do
       post users_url, params: { user: { age: @user.age, firstname: @user.firstname, lastname: @user.lastname, sex: @user.sex } }
     end
 
@@ -39,7 +41,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy user" do
-    assert_difference('User.count', -1) do
+    assert_difference("User.count", -1) do
       delete user_url(@user)
     end
 
